5a9e8f15e6bacf1595a646a03980a98cae51a04b
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-04-26 16:02:23 +0300

    delete demo pages

--- reflex_app/reflex_app.py
+++ reflex_app/reflex_app.py
@@ -5,8 +5,6 @@ from reflex_app.pages import *
 
 import reflex as rx
 
-from reflex_app.pages import example
-
 
 class State(rx.State):
     """Define empty state to allow access to rx.State.router."""
