85554d48fc1dcffeb567c4aa293c467fd68f937e
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-04-28 19:53:45 +0300

    update styles

--- reflex_app/styles.py
+++ reflex_app/styles.py
@@ -10,7 +10,7 @@ accent_color = rx.color("accent", 1)
 hover_accent_color = {"_hover": {"color": accent_text_color}}
 hover_accent_bg = {"_hover": {"background_color": accent_color}}
 content_width_vw = "90vw"
-sidebar_width = "20em"
+
 
 template_page_style = {
     "padding_top": "2em",
@@ -24,11 +24,7 @@ template_content_style = {
     "padding": "1em",
 }
 
-link_style = {
-    "color": accent_text_color,
-    "text_decoration": "none",
-    **hover_accent_color,
-}
+link_style = {}
 
 overlapping_button_style = {
     "background_color": "white",
@@ -38,11 +34,4 @@ overlapping_button_style = {
 markdown_style = {
     "code": lambda text: rx.code(text, color_scheme="gray"),
     "codeblock": lambda text, **props: rx.code_block(text, **props, margin_y="1em"),
-    "a": lambda text, **props: rx.link(
-        text,
-        **props,
-        font_weight="bold",
-        text_decoration="underline",
-        text_decoration_color=accent_text_color,
-    ),
 }
